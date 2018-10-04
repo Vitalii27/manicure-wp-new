53d3174dec9ed93d198cf1322649461b84a17baf
Vitalii27 <v.khlystun01@gmail.com>
2018-10-04 17:56:44 +0300

    /

--- wp-content/themes/manicure/src/js/select-js.js
+++ wp-content/themes/manicure/src/js/select-js.js
@@ -1,8 +1,8 @@
-jQuery(function ($) {
-$('.form-select').attr('title', 'jnjn');
-    $('.form-select').selectpicker({
-        style: 'btn-info',
-        size: 10,
-        title: 'Выберите доп. курс со скидкой'
-    });
-});
+// jQuery(function ($) {
+// $('.form-select').attr('title', 'jnjn');
+//     $('.form-select').selectpicker({
+//         style: 'btn-info',
+//         size: 10,
+//         title: 'Выберите доп. курс со скидкой'
+//     });
+// });
