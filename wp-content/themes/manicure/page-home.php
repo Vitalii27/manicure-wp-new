ba0f68eeef29f9448c1da6539f977f5b9bd6c298
Vitalii27 <v.khlystun01@gmail.com>
2018-10-04 17:03:25 +0300

    /

--- wp-content/themes/manicure/page-home.php
+++ wp-content/themes/manicure/page-home.php
@@ -14,19 +14,20 @@ get_header(); ?>
 <?php get_template_part( './template-parts/sections/hero' ); ?>
 <?php get_template_part( './template-parts/sections/sub-section' ); ?>
 <?php //get_template_part( './template-parts/sections/section-pink' ); ?>
-<?php get_template_part( './template-parts/sections/super' ); ?>
+
 
 <?php get_template_part( './template-parts/sections/action' ); ?>
 
 
-<?php get_template_part( './template-parts/sections/partners' ); ?>
 
-<?php get_template_part( './template-parts/sections/grafic' ); ?>
 <?php get_template_part( './template-parts/sections/program' ); ?>
+<?php //get_template_part( './template-parts/sections/super' ); ?>
 <?php get_template_part( './template-parts/sections/diplom' ); ?>
 <?php get_template_part( './template-parts/sections/sing-up' ); ?>
 
 <?php get_template_part( './template-parts/sections/advantages' ); ?>
+<?php get_template_part( './template-parts/sections/partners' ); ?>
+
 <?php get_template_part( './template-parts/sections/about' ); ?>
 <?php get_template_part( './template-parts/sections/realy-study' ); ?>
 <?php get_template_part( './template-parts/sections/teachers' ); ?>
@@ -35,6 +36,8 @@ get_header(); ?>
 <?php get_template_part( './template-parts/sections/info' ); ?>
 
 <?php get_template_part( './template-parts/sections/photo' ); ?>
+<?php get_template_part( './template-parts/sections/grafic' ); ?>
+
 <?php get_template_part( './template-parts/sections/text' ); ?>
 <?php get_template_part( './template-parts/sections/contacts' ); ?>
 
